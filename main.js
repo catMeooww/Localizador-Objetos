f20e9a10166d4b335a6f7c53520de29722ffbcd1
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-08-31 15:37:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+var detect = "";
+var Status = false;
 function setup(){
     canvas = createCanvas(330,300);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function Start(){
+    objectDetector = ml5.objectDetector("cocoSSD",modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Identificando Objetos";
+    detect = document.getElementById("OInput").value;
+}
+function modelLoaded(){
+    Status = true;
+    console.log("Modelo Carregado");
+}
+function draw(){
+    image(video,0,0,330,300);
 }
